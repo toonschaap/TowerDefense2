acf1c67c040c5884ea405465177868e0e3bfad01
ToonSchaap <toonschaap3@gmail.com>
2018-11-13 12:35:48 +0100

    first
    1

---
+++ Assets/scripts/player/clickspawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// © 2017 TheFlyingKeyboard and released under MIT License
+// theflyingkeyboard.net
+public class clickspawn : MonoBehaviour {
+	public GameObject objectToSpawn;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			spawnPosition.z = 0.0f;
+			GameObject objectInstance = Instantiate(objectToSpawn, spawnPosition, Quaternion.Euler(new Vector3(0, 0, 0)));
+		}
+	}
+}
